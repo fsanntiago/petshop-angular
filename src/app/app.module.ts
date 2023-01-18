d530194a171b4984d576ab5ac67a910bf4e7d611
fsanntiago <zerofabricio@hotmail.com>
2023-01-18 20:23:25 -0300

    feat: add reset password page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
+import { ResetPasswordPageComponent } from "./reset-password-page/reset-password-page.component";
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LoginPageComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
