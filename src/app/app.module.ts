d4f58b2b5f74dcae5125bbfc4177fc990ee23099
fsanntiago <zerofabricio@hotmail.com>
2023-01-19 09:03:45 -0300

    feat: add signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { ResetPasswordPageComponent } from "./reset-password-page/reset-password-page.component";
+import { SignupPageComponent } from "./signup-page/signup-page.component";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ResetPasswordPageComponent } from "./reset-password-page/reset-password
     NavbarComponent,
     LoginPageComponent,
     ResetPasswordPageComponent,
+    SignupPageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
