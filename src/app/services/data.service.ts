dd4b3837ca7ca919d38e809b63360f7fd4da715d
fsanntiago <zerofabricio@hotmail.com>
2023-01-22 13:49:50 -0300

    feat: add data service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+// posso injetar essa clase em outras classes
+// 'root' = disponivel para todos os modulos
+@Injectable({ providedIn: "root" })
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+    return this.http.get<any[]>("http://localhost:3000/v1/products");
+  }
+}
