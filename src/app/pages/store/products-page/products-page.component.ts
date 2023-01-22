108178895c7b5a0faa77d96e1de6b2fcb15265f3
fsanntiago <zerofabricio@hotmail.com>
2023-01-22 16:22:33 -0300

    feat: add product card component

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Product } from "src/app/models/product.model";
+import { DataService } from "./../../../services/data.service";
 
 @Component({
   selector: "app-products-page",
   templateUrl: "./products-page.component.html",
 })
 export class ProductsPageComponent implements OnInit {
-  constructor() {}
+  public products$: Observable<Product[]>;
 
-  ngOnInit() {}
+  constructor(private data: DataService) {}
+
+  ngOnInit() {
+    this.products$ = this.data.getProducts();
+  }
 }
