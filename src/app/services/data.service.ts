239fd00c1f02f7171510c600d0b919e396c32592
fsanntiago <zerofabricio@hotmail.com>
2023-01-23 09:42:48 -0300

    feat: add authenticate

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,10 +5,18 @@ import { Injectable } from "@angular/core";
 // posso injetar essa clase em outras classes
 // 'root' = disponivel para todos os modulos
 @Injectable({ providedIn: "root" })
+
+// pode ter multiplos services. Ex: ProductServices, AccountServices
 export class DataService {
+  public url = "http://localhost:3000/v1";
+
   constructor(private http: HttpClient) {}
 
   getProducts() {
-    return this.http.get<Product[]>("http://localhost:3000/v1/products");
+    return this.http.get<Product[]>(`${this.url}/product`);
+  }
+
+  authenticate(data) {
+    return this.http.post(`${this.url}/accounts/authenticate`, data);
   }
 }
