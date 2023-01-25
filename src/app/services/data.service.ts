b615ef4526683b7ec5ec98d74e7635c4864eac8f
fsanntiago <zerofabricio@hotmail.com>
2023-01-25 10:54:00 -0300

    feat: add reset-password

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -35,4 +35,8 @@ export class DataService {
   create(data) {
     return this.http.post(`${this.url}/accounts`, data);
   }
+
+  resetPassword(data) {
+    return this.http.post(`${this.url}/accounts/reset-password`, data);
+  }
 }
